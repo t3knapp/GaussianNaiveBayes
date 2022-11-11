e54ad000ad718cf56f3411c031c05735aa5dc0c7
Tyler Knapp <t3knapp@gmail.com>
2022-11-11 14:28:18 -0800

    Completed V1. Need to test and bugfix.

--- GaussianNaiveBayes.py
+++ GaussianNaiveBayes.py
@@ -1,6 +1,4 @@
 import numpy as np
-import statistics as stat
-import math
 
 class GaussianNaiveBayes:
     """
@@ -8,7 +6,7 @@ class GaussianNaiveBayes:
     ...
     Parameters
     _________
-    prior : string
+    prior_ : string
         The prior distribution of the classes.
 
     Attributes
@@ -21,8 +19,12 @@ class GaussianNaiveBayes:
         Number of unique class labels in the training set y
         Represents the number of categories we are sorting our data into
 
-    priors_: array-like, shape = [1, n_labels]
+    labels_: array like, shape = [1, n_labels]
+        Unique labels from the training set
 
+    prior_type_: array-like, shape = [1, n_labels]
+        Prior distribution of our test data
+        We assume this is uniform for now
 
     data_: list of array-likes, shape = [n_labels, varies, n_cols]
         A list of matrices that contain the training data sorted by class label
@@ -35,7 +37,7 @@ class GaussianNaiveBayes:
     train(X_train, y_train) :
         Creates the parameters mean and var for each category of the data
 
-    test(X_test, y_test) :
+    predict(X_test, y_test) :
         Uses the parameters from train to predict labels and checks for accuracy
 
     gaussian(x, mean, var):
@@ -43,8 +45,8 @@ class GaussianNaiveBayes:
 
     """
 
-    def __init__(self, prior='uniform'):
-        self.prior = prior
+    def __init__(self, prior_type_='uniform'):
+        self.prior_type_ = prior_type_
 
     def fit(self, X, y):
         """
@@ -59,52 +61,121 @@ class GaussianNaiveBayes:
         Returns:
             self: object
         """
-        #Sorts the data into a list of arrays data.
-        #Each array in data is the training set for a given class label
-        self.sort_data(self, X, y)
+        # Sorts the data into a list of arrays data.
+        # Each array in data is the training set for a given class label
+        data_ = self._sort_data(X, y)
 
         # Calculate mean and variance of data and store in params
-        for i in range(0, len(self.data_)):
-            for j in range(0, self.data_[i].shape[0]):
-                self.params_[i, 0, j] = np.mean(self.data_[i][:, j])
-                self.params_[i, 1, j] = np.var(self.data_[i][:, j])
+        for i in range(0, len(data_)):
+            for j in range(0, data_[i].shape[1]):
+                self.params_[i, 0, j] = np.mean(data_[i][:, j])
+                self.params_[i, 1, j] = np.var(data_[i][:, j])
         return self
 
-    def test(self, X, y):
+    def predict(self, X, y):
+        """
+                Testing our stored parameters using a test dataset
+
+                Parameters:
+                    X: array-like, shape = [n_samples, n_features]
+                        Test vectors
+                    y: array-like, shape = [n_samples]
+                        Target values from test set
+
+                Returns:
+                    predict: array-like, shape = [n_samples]
+                        Prediction associated with each test vector
+                """
+        # If prior is uniform, create the prior
+
         # Creating likelihoods for each class label
         likelihoods = []
-        for i in range(0, self.n_labels):
-            temp_likelihood = np.zeros(1, X.shape[1])
-            for j in range(0, self.data_[i].shape[0]):
-                for k in range(0, self.data_[i].shape[1]):
-                    temp_likelihood[j] = self.gaussian(X[j, k], self.params_[i, 0, j], self.params_[i, 1, j])
+        for i in range(0, self.n_labels_):
+            likelihoods.append(self._gaussian(X, self.params_[i, 0, :], self.params_[i, 1, :]))
+
+        # Using likelihoods and prior with N-bayes formula
+        # Post is a list of the posterior probabilities separated by label
+        post = np.zeros(X.shape[0], self.n_labels_)
+        predict = np.zeros(X.shape[0], 1)
+        for i in range(0, self.n_labels_):
+            # Multiply across rows of likelihoods and multiply by the prior
+            post[:, i] = self.prior_[i] * np.prod(likelihoods[i], axis=0)
+            # Find the largest posterior and use index to tie it to a label
+            predict[i] = self.labels_[np.argmax(post[:, i])]
+
+        # Calculating the percentage of correct predictions
+        correct = 0
+        for i in range(0, len(y)):
+            if predict[i] == y[i]:
+                correct += 1
+
+        percent_correct = correct / len(y)
+        print("The prediction is " + str(percent_correct) + "% correct")
+        return predict
 
 
+    def _sort_data(self, X, y):
+        """
+                Takes the training data and separates it by label
 
-    def sort_data(self, X, y):
+                Parameters:
+                    X: array-like, shape = [n_samples, n_features]
+                        Training vectors
+                    y: array-like, shape = [n_samples]
+                        Target values from training set
+
+                Returns:
+                    data_: list of array-likes, shape = [n_labels][n_samples_in_label, n_features]
+                        A list of np arrays, containing the training data separated by label in corresponding y
+                """
         # We first need to separate our data by class label
-        labels = np.unique(y)
-        self.n_labels = len(labels)
+        self.labels_ = np.unique(y)
+        self.n_labels_ = len(self.labels_)
         n_cols = X.shape[1]
 
         # Creates n_labels matrices holding the mean and variance for each column
-        self.params_ = np.zeros((self.n_labels, 2, n_cols))
+        self.params_ = np.zeros((self.n_labels_, 2, n_cols))
 
+        X = np.append(X, y, axis=1)
         # Data will contain arrays of training vectors separated by class
         # Data is a list of numpy arrays
-        self.data_ = []
+        data_ = []
         for i in range(0, len(labels)):
-            X_temp = []
-            for j in range(0, len(y)):
-                if y[j] == labels[i]:
-                    X_temp = np.vstack(X_temp, X[i, :])
-            self.data_.append(X_temp)
+            mask = (X[:, -1] == labels[i])
+            data_.append(X[mask, :])
+        return data_
+
+    def _gaussian(self, x, mean, var):
+        """
+                Calculates the likelihoods based on a Gaussian distribution
+
+                Parameters:
+                    x: array-like, shape = [n_samples, 1]
+                        Test vectors
+                    mean: double
+                        mean for the given data point
+                    var: double
+                        variance for the given data points
+
+                Returns:
+                    Gauss: array-like, shape
+                        likelihoods for the datapoints from x
+                """
+        return (1/np.sqrt(2*np.pi*var))*np.exp(-(x-mean)*(x-mean)/(2*var))
+
+    def _create_prior(self):
+        """
+                Creates the prior distribution from string user input
+                Currently only works for a uniform distribution
+
+                Parameters:
+                    self: object
+
+                Returns:
+                    self: object
+                """
+        if self.prior_type_ == 'uniform':
+            self.prior_ = (1 / self.n_labels_) * np.ones((1, self.n_labels_))
+        else:
+            raise Exception("Must set prior distribution to uniform.")
         return self
-
-    def gaussian(self, x, mean, var):
-        return (1/math.sqrt(2*math.pi*var))*math.exp(-(x-mean)*(x-mean)/(2*var))
-
-    def create_prior(self):
-        if self.prior=='uniform':
-            return
-
